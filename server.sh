4bd52d6eefa447da94437e03940c25cbef89b42b
fmh <abed.ensa@uhp.ac.ma>
2021-03-30 16:06:49 +0200

    adding fetch all branches

--- server.sh
+++ server.sh
@@ -3,7 +3,13 @@ str="'$*'"
 array="${@}"
 
 echo $array
-git add . && git commit -m "$array"
+git add . && git commit -a -m "$array"
 
 
 git push origin --all
+
+# fetch all branches from remote
+
+# for b in `git branch -r | grep -v -- '->'`; do git branch --track ${b##origin/} $b; done
+
+# git fetch --all
